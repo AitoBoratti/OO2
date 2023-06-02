4455515a0cdeb86dc66387a97127278867aecdd6
Alberto Boratti <albertoboratti@gmail.com>
2023-06-02 15:53:13 -0300

    Agregado ejercicio 10

---
+++ Practica/Ejercicio10AdministradorProyectos/src/main/java/ar/edu/unlp/info/oo1/Confirmada.java
@@ -0,0 +1,16 @@
+package ar.edu.unlp.info.oo1;
+
+public class Confirmada extends state{
+
+    public Confirmada(Proyecto proyecto) {
+        super(proyecto);
+    }
+    @Override
+    void AprobarEtapa() {
+
+    }
+    @Override
+    void ModificarMargenGanancia(int margen) {
+
+    }
+}
